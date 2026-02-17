3b41eedf5939dd75119919d0338d77ed558d655d
Declan Chidlow <accounts@vale.rocks>
2026-02-17 16:27:19 +0800

    Simple sidebar implementation (WIP)

---
+++ src/assets/scripts/docs-sidebar.js
@@ -0,0 +1,16 @@
+const sidebar = document.getElementById("sidebar-menu");
+const contentsButton = document.getElementById("contents-button");
+const mediaQuery = window.matchMedia("(max-width: 45rem)");
+
+function handleDisplaySize(e) {
+	if (e.matches) {
+		contentsButton.style.display = "flex";
+		sidebar.setAttribute("popover", "auto");
+	} else {
+		sidebar.removeAttribute("popover");
+		contentsButton.style.display = "none";
+	}
+}
+
+mediaQuery.addEventListener("change", handleDisplaySize);
+handleDisplaySize(mediaQuery);
