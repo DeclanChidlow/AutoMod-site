a0e782d46f83fcce81a9380013bbcf02fb949fe2
Declan Chidlow <accounts@vale.rocks>
2026-07-13 14:14:11 +0800

    Fix worker

--- worker.js
+++ worker.js
@@ -1,5 +1,11 @@
-const url = new URL(request.url);
-if (url.pathname.startsWith("/dashboard")) {
-	const dashPath = url.pathname.slice("/dashboard".length) || "/";
-	return fetch(`https://dash.automod.vale.rocks${dashPath}${url.search}`, request);
-}
+export default {
+	async fetch(request, env) {
+		const url = new URL(request.url);
+
+		if (url.pathname.startsWith("/dashboard")) {
+			return fetch(`https://dash.automod.vale.rocks${url.pathname}${url.search}`, request);
+		}
+
+		return env.ASSETS.fetch(request);
+	},
+};
